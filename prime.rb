d972ce04e992754b953db01b9335eb73706aee90
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-10 22:51:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 require 'pry'
 # Add  code here!
 def prime?(num)
-  if num > 1 && (num / num == 1) && num % num !!= 0
+  if num > 1 && (num / num == 1) && num % num != 0
     true
   else
     false
