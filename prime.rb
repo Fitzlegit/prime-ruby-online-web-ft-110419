0dc0f1644ae48a8b5ea377a969bd3d8e239dd23e
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-10 23:01:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,6 @@ def prime?(num)
   if num < 0 || num == 0 || num == 1
     return false
   else
-    (2..num).to_a.all?{|factor| num % factor != 0}
+    (2..num-1).to_a.all?{|factor| num % factor != 0}
   end
 end
